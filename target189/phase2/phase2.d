f186e109a39bd13fd229160e494af5189fdc6aea
yuxmi <mnakajim@andrew.cmu.edu>
2023-02-15 21:43:35 -0500

    phase2

---
+++ target189/phase2/phase2.d
@@ -0,0 +1,9 @@
+
+phase2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 66 50 f0 3a 	mov    $0x3af05066,%rdi
+   7:	c3                   	ret    
---
+++ target189/phase2/phase2.s
@@ -0,0 +1,2 @@
+mov $0x3af05066, %rdi
+retq
