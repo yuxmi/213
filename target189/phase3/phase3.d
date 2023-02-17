06ccacbde30805f0d3d49eb698c060367dcbd5b9
yuxmi <mnakajim@andrew.cmu.edu>
2023-02-16 21:54:43 -0500

    phase3

--- target189/phase3/phase3.d
+++ target189/phase3/phase3.d
@@ -5,5 +5,5 @@ phase3.o:     file format elf64-x86-64
 Disassembly of section .text:
 
 0000000000000000 <.text>:
-   0:	48 c7 07 5c 7b 64 55 	movq   $0x55647b5c,(%rdi)
+   0:	48 c7 c7 68 7b 64 55 	mov    $0x55647b68,%rdi
    7:	c3                   	ret    
--- target189/phase3/phase3.s
+++ target189/phase3/phase3.s
@@ -1,2 +1,2 @@
-movq $0x55647b5c, (%rdi)
+movq $0x55647b68, %rdi
 retq
